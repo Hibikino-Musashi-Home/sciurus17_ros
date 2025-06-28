5e139ce62d7e80a5203796616e7839feaf296c09
yoshimura04 <yoshimura.wataru281@mail.kyutech.jp>
2025-06-28 20:50:59 +0900

    左アームを制御するサーバとクライアントを追加（joint角度指定）

--- sciurus17_examples/README.md
+++ sciurus17_examples/README.md
@@ -311,3 +311,16 @@ ros2 run sciurus17_examples cartesian_pose_right_arm_waist_server
 ```bash
 ros2 run sciurus17_examples cartesian_pose_right_arm_waist_client.py
 ```
+
+## 左アーム制御（joint角指定）
+以下のコマンドは、Sciurus17の左アームを制御するためのROS 2コマンドです。  
+各jointの角度を指定します。  
+ステートマシン内で制御するには、client.pyのクラスをステートマシン内でインスタンス化し、send_pose_goal関数を呼び出します。
+### 左アームを制御するサーバーを起動
+```bash
+ros2 run sciurus17_examples set_joint_values_left_arm_waist_server
+```
+### クライアントを起動し、各jointを指定した角度にする
+```bash
+ros2 run sciurus17_examples cartesian_pose_right_arm_waist_client.py
+```
---
+++ sciurus17_examples/scripts/set_joint_values_left_arm_waist_client.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from rclpy.action import ActionClient
+from sciurus17_interfaces.action import SetJointValues
+import sys
+
+class SetJointValuesLeftArmWaist(Node):
+    def __init__(self):
+        super().__init__('set_joint_values_client')
+        self._client = ActionClient(self, SetJointValues, 'set_joint_values_left_arm_waist')
+
+    def send_goal(self, joint_values):
+        self.get_logger().info('Waiting for action server...')
+        self._client.wait_for_server()
+
+        goal_msg = SetJointValues.Goal()
+        goal_msg.joint_values = joint_values
+
+        self.get_logger().info(f'Sending joint values: {joint_values}')
+        send_goal_future = self._client.send_goal_async(goal_msg)
+        rclpy.spin_until_future_complete(self, send_goal_future)
+
+        goal_handle = send_goal_future.result()
+        if not goal_handle.accepted:
+            self.get_logger().error('Goal rejected')
+            return False
+
+        self.get_logger().info('Goal accepted. Waiting for result...')
+        get_result_future = goal_handle.get_result_async()
+        rclpy.spin_until_future_complete(self, get_result_future)
+
+        result = get_result_future.result().result
+        self.get_logger().info(f'Result: success={result.success}, message="{result.message}"')
+
+        return result.success
+
+
+def main():
+    rclpy.init()
+    client = SetJointValuesLeftArmWaist()
+
+    # 任意の関節角度（ラジアン）
+    joint_values = [0.0536893, 0.374291, 1.58767, -0.0644272, -2.73202, -0.0276117, 1.75334, 0.107379]
+
+    success = client.send_goal(joint_values)
+    client.destroy_node()
+    rclpy.shutdown()
+
+    if not success:
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
